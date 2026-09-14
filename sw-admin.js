8b3baef86adb985a92fa10c7bf5f9365d7142cb5
recargasGames <aa39604a@gmail.com>
2026-09-14 13:18:30 -0400

    Add service worker for caching admin resources

---
+++ sw-admin.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'admin-rg-v1';
+const urlsToCache = ['/admin.html', '/logo.png'];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then(names =>
+            Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
+});
